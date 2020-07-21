04eb7e715b41edaeffc44e90221b45627f19f2b7
mariariazanova <mv_13@mail.ru>
2020-07-21 22:40:16 +0300

    1

---
+++ RainbowFrame copy/components/RainbowFrame.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+      colors:PropTypes.array,
+  };
+
+  render() {
+
+    
+    
+    return (
+      (this.props.colors.length)
+      ? <div style={{border: "solid 8px " + this.props.colors[0], padding: "8px"}}>
+              <RainbowFrame colors={this.props.colors.splice(1)}>
+                  {this.props.children}
+              </RainbowFrame>
+         </div>
+      : <span>{this.props.children}</span>
+     
+    );
+  }
+
+
+}
+
+export default RainbowFrame;    
+
