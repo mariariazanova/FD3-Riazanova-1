a4464c38bd001a53105622cdef23d228d3979ab8
mariariazanova <mv_13@mail.ru>
2020-07-22 10:46:20 +0300

    modified:   bundle.css 	modified:   bundle.js 	modified:   bundle.js.map

--- RainbowFrame/bundle.js
+++ RainbowFrame/bundle.js
@@ -29161,7 +29161,7 @@ exports.unstable_wrap = unstable_wrap;
 
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -29185,44 +29185,40 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var RainbowFrame = function (_React$Component) {
-  _inherits(RainbowFrame, _React$Component);
+    _inherits(RainbowFrame, _React$Component);
 
-  function RainbowFrame() {
-    _classCallCheck(this, RainbowFrame);
+    function RainbowFrame() {
+        _classCallCheck(this, RainbowFrame);
 
-    return _possibleConstructorReturn(this, (RainbowFrame.__proto__ || Object.getPrototypeOf(RainbowFrame)).apply(this, arguments));
-  }
-
-  _createClass(RainbowFrame, [{
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
-
-      //var rb=this.props.colors.forEach( color => { 
-      //    <div style={{border:"solid 5px "+this.props.color,padding:"10px"}}>
-      //   </div>
-      //     });  
-
-
-      return _react2.default.createElement(
-        'div',
-        { className: 'Rainbow' },
-        this.props.colors.forEach(function (color) {
-          return _react2.default.createElement('div', { style: { border: "solid 5px " + _this2.props.color, padding: "10px" } });
-        }, _react2.default.createElement(
-          'p',
-          null,
-          this.props.children
-        ))
-      );
+        return _possibleConstructorReturn(this, (RainbowFrame.__proto__ || Object.getPrototypeOf(RainbowFrame)).apply(this, arguments));
     }
-  }]);
 
-  return RainbowFrame;
+    _createClass(RainbowFrame, [{
+        key: 'render',
+        value: function render() {
+
+            var rainbow = this.props.children;
+            this.props.colors.forEach(function (color) {
+                return rainbow = _react2.default.createElement(
+                    'div',
+                    { style: { border: "solid 8px " + color, padding: "8px" } },
+                    rainbow
+                );
+            });
+
+            return _react2.default.createElement(
+                'div',
+                { className: 'Rainbow' },
+                rainbow
+            );
+        }
+    }]);
+
+    return RainbowFrame;
 }(_react2.default.Component);
 
 RainbowFrame.propTypes = {
-  colors: _propTypes2.default.array.isRequired
+    colors: _propTypes2.default.array.isRequired
 };
 exports.default = RainbowFrame;
 
