6198ddca83a4733b7234754a06a945477cd1d909
mariariazanova <mv_13@mail.ru>
2020-07-28 00:13:23 +0300

    Merge branch 'master' of https://github.com/mariariazanova/FD3-Riazanova-1

--- RainbowFrame copy/components/RainbowFrame.js
+++ RainbowFrame copy/components/RainbowFrame.js
@@ -16,7 +16,7 @@ class RainbowFrame extends React.Component {
     return (
       (this.props.colors.length)
       ? <div style={{border: "solid 8px " + this.props.colors[0], padding: "8px"}}>
-              <RainbowFrame colors={this.props.colors.splice(1)}>  { /* <передаем массив цветов, кроме первого/> */ }
+              <RainbowFrame colors={this.props.colors.splice(1)}> { /* <передаем массив цветов, кроме первого/> */ }
                   {this.props.children}
               </RainbowFrame>
          </div>
@@ -24,6 +24,8 @@ class RainbowFrame extends React.Component {
      
     );
   }
+
+
 
 
 }
